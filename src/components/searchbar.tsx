318994152680bd0abdcea3b282c63a16f109b4d4
Park-Jingyeong <pjg0305pjg@gmail.com>
2025-02-09 22:08:54 +0900

    Feat: create component searchbar

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -10,17 +10,26 @@ export default function SearchBar() {
     setSearch(e.target.value);
   };
   const onSubmit = () => {
+    if(!search) return;
     router.push(`/search?q=${search}`);
+  };
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
   };
   return (
     <div>
       <input
         value={search}
         onChange={onChangeInput}
+        onKeyDown={onKeyDown}
         placeholder="장소를 입력해주세요"
         className="border-2"
       />
-      <button onClick={onSubmit} className="border-2">검색</button>
+      <button onClick={onSubmit} className="border-2">
+        검색
+      </button>
     </div>
   );
 }
