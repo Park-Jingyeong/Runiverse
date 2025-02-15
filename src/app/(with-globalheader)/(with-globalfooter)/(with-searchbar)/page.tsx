9643b5f13e3f7b785a16c579112e6b123d4d9672
Park-Jingyeong <pjg0305pjg@gmail.com>
2025-02-15 21:10:03 +0900

    Feat: index page setting

--- src/app/(with-globalheader)/(with-globalfooter)/(with-searchbar)/page.tsx
+++ src/app/(with-globalheader)/(with-globalfooter)/(with-searchbar)/page.tsx
@@ -1,3 +1,39 @@
 export default function Home() {
-  return <div>index 페이지(메인)</div>;
+  return (
+    <div className="flex flex-col gap-8">
+      <section>
+        <h1 className="bold text-xl">Slider Section</h1>
+        <div>course component</div>
+      </section>
+      <section>
+        <button className="bg-[#4BB7D4] w-80 h-10 rounded-lg">
+          오늘의 러닝 코스 추천받기
+        </button>
+      </section>
+      <section>
+        <div className="flex justify-between">
+          <h1 className="bold text-xl">추천 많은 코스</h1>
+          <button>더보기</button>
+        </div>
+        <div className="flex flex-col gap-3">
+          <div>course component 1</div>
+          <div>course component 2</div>
+          <div>course component 3</div>
+        </div>
+      </section>
+      <section>
+        <h1 className="bold text-xl">지역별 추천 코스</h1>
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between">
+            <button>city - link</button>
+            <button>city - link</button>
+          </div>
+          <div className="flex justify-between">
+            <button>city - link</button>
+            <button>city - link</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
