8e7a913890f77526117a6f78bf9450c7f61d74e6
Park-Jingyeong <pjg0305pjg@gmail.com>
2025-04-15 18:12:34 +0900

    Feat: onLoad 삭제

--- src/components/mapSearch.tsx
+++ src/components/mapSearch.tsx
@@ -13,107 +13,116 @@ declare global {
 export default function MapSearch() {
   const [map, setMap] = useState<any>(null);
   const [keyword, setKeyword] = useState("");
+  const [clickedAddress, setClickedAddress] = useState("");
   const mapRef = useRef<HTMLDivElement>(null);
-  const infoRef = useRef<HTMLDivElement>(null)
   const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
   };
 
   useEffect(() => {
-    const onLoadKakaoMap = () => {
-      window.kakao.maps.load(() => {
-        // 지도 container
-        const mapContainer = mapRef.current;
-        // 지도 중심 좌표와 확대 레벨
-        const mapOption = {
-          center: new window.kakao.maps.LatLng(37.566826, 126.9786567),
-          level: 3,
-        };
+    window.kakao.maps.load(() => {
+      // 지도 container
+      const mapContainer = mapRef.current;
+      // 지도 중심 좌표와 확대 레벨
+      const mapOption = {
+        center: new window.kakao.maps.LatLng(37.566826, 126.9786567),
+        level: 3,
+      };
 
-        // 지도 생성
-        const mapInstance = new window.kakao.maps.Map(mapContainer, mapOption);
-        setMap(mapInstance);
+      // 지도 생성
+      const mapInstance = new window.kakao.maps.Map(mapContainer, mapOption);
+      setMap(mapInstance);
 
-        //
+      //
 
-        // 주소-좌표 변환 객체 생성
-        const geocoder = new window.kakao.maps.services.Geocoder();
+      // 주소-좌표 변환 객체 생성
+      const geocoder = new window.kakao.maps.services.Geocoder();
 
-        // 클릭한위치를 표시할 마커
-        const marker = new window.kakao.maps.Marker();
-        // 클릭한 위치에 대한 주소를 표시할 인포윈도우
-        const infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
+      // 클릭한위치를 표시할 마커
+      const marker = new window.kakao.maps.Marker();
+      // 클릭한 위치에 대한 주소를 표시할 인포윈도우
+      const infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
 
-        // 현재 지도 중심 좌표로 주소를 검색해서 지도 좌측 상단에 표시 (행정구역 정보?)
-        searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+      // 현재 지도 중심 좌표로 주소를 검색해서 지도 좌측 상단에 표시 (행정구역 정보?)
+      // searchAddrFromCoords(map.getCenter(), displayCenterInfo);
 
-        // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소 정보를 표시하는 이벤트 등록
-        window.kakao.maps.event.addListener(
-          map,
-          "click",
-          function (mouseEvent: any) {
-            searchDetailAddrFromCoords(
-              mouseEvent.latLng,
-              function (result: any, status: any) {
-                if (status === window.kakao.maps.services.Status.OK) {
-                  const detailAddr = !!result[0].road_address
-                    ? "<div>도로명 주소 : " +
-                      result[0].road_address.address_name +
-                      "</div>" +
-                      "<div>지번 주소 : " +
-                      result[0].address.address_name +
-                      "</div>"
-                    : "";
-                  const content =
-                    '<div class="bAddr">' +
-                    '<span class="title">법정동 주소정보</span>' +
-                    detailAddr +
-                    "</div>";
-
-                  // 마커를 클릭한 위치에 표시
-                  marker.setPosition(mouseEvent.latLng);
-                  marker.setMap(map);
-
-                  // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소 정보 표시
-                  infowindow.setContent(content);
-                  infowindow.open(map, marker);
-                }
-              }
-            );
-          }
-        );
-
-        window.kakao.maps.event.addListener(map, "idle", function () {
-          searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-        });
-
-        function searchAddrFromCoords(coords: any, callback: any) {
-          // 좌표로 행정동 주소 정보를 요청
-          geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
-        }
-        function searchDetailAddrFromCoords(coords: any, callback: any) {
-          // 좌표로 법정동 상세 주소 정보를 요청
-          geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
-        }
-
-        function displayCenterInfo(result: any, status: any) {
-          if (status === window.kakao.maps.services.Status.OK) {
-            // const infoDiv = document.getElementById("centerAddr");
-            const infoDiv = infoRef.current;
-
-            for (let i = 0; i < result.length; i++) {
-              // 행정동의 region_type 값은 'H'
-              if (result[i].region_type === "H") {
-                infoDiv.innerHTML = result[i].address_name;
-                break;
+      // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소 정보를 표시하는 이벤트 등록
+      window.kakao.maps.event.addListener(
+        mapInstance,
+        "click",
+        function (mouseEvent: any) {
+          const latlng = mouseEvent.latlng;
+          console.log(latlng);
+          if (!latlng) return;
+          geocoder.coord2Address(
+            latlng.getLng(),
+            latlng.getLat(),
+            function (result: any, status: any) {
+              if (status === window.kakao.maps.services.Status.OK) {
+                const address = result[0].address.address_name;
+                setClickedAddress(address);
               }
             }
-          }
-        }
-      });
-    };
+          );
 
-    onLoadKakaoMap();
+          searchDetailAddrFromCoords(
+            mouseEvent.latLng,
+            function (result: any, status: any) {
+              if (status === window.kakao.maps.services.Status.OK) {
+                const detailAddr = !!result[0].road_address
+                  ? "<div>도로명 주소 : " +
+                    result[0].road_address.address_name +
+                    "</div>" +
+                    "<div>지번 주소 : " +
+                    result[0].address.address_name +
+                    "</div>"
+                  : "";
+                const content =
+                  '<div class="bAddr">' +
+                  '<span class="title">법정동 주소정보</span>' +
+                  detailAddr +
+                  "</div>";
+
+                // 마커를 클릭한 위치에 표시
+                marker.setPosition(mouseEvent.latLng);
+                marker.setMap(map);
+
+                // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소 정보 표시
+                infowindow.setContent(content);
+                infowindow.open(map, marker);
+              }
+            }
+          );
+        }
+      );
+
+      // window.kakao.maps.event.addListener(map, "idle", function () {
+      //   searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+      // });
+
+      function searchAddrFromCoords(coords: any, callback: any) {
+        // 좌표로 행정동 주소 정보를 요청
+        geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+      }
+      function searchDetailAddrFromCoords(coords: any, callback: any) {
+        // 좌표로 법정동 상세 주소 정보를 요청
+        geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+      }
+
+      // function displayCenterInfo(result: any, status: any) {
+      //   if (status === window.kakao.maps.services.Status.OK) {
+      //     const infoDiv = document.getElementById("centerAddr");
+
+      //     for (let i = 0; i < result.length; i++) {
+      //       // 행정동의 region_type 값은 'H'
+      //       if (result[i].region_type === "H") {
+      //         infoDiv.innerHTML = result[i].address_name;
+      //         break;
+      //       }
+      //     }
+      //   }
+      // }
+    });
   }, []);
 
   const handleSearch = (e: React.MouseEvent<HTMLButtonElement>) => {
