c47e9d3ab32e4823fcdee23f661820fcabafc050
Park-Jingyeong <pjg0305pjg@gmail.com>
2025-04-24 12:02:53 +0900

    Style: randomCourse 이미지 margin 적용

--- src/app/(with-globalheader)/page.tsx
+++ src/app/(with-globalheader)/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       <div className="flex flex-col gap-8">
         <section>
           <div>
-            <div className="">
+            <div className="-mx-4">
               {randomCourse?.imageUrls?.[0] && (
                 <Image
                   src={randomCourse.imageUrls[0]}
